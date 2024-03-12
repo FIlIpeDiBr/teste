aa399f8fa62581ca772f54fad8a585500bd1891e
Filipe Dias Brandão <filipedbrandao1@gmail.com>
2024-03-12 15:59:14 -0400

    Exclusão do scs

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ export default defineConfig({
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'node_modules/jquery/dist/jquery.js',
-                'node_modules/bootstrap/scss/bootstrap.scs',
                 'node_modules/bootstrap/dist/css/bootstrap.css',
                 'node_modules/bootstrap/dist/js/bootstrap.bundle.js'
             ],
